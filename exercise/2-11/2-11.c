b2f69f1a83ca7fafc77b51e71b362bf1ef4bd8ca
pun <koonimpan555@gmail.com>
2022-11-22 19:48:32 +0700

    add:exercies2-11

---
+++ exercise/2-11/2-11.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, ans, series = 9;
+    printf("User Input : ");
+    scanf("%d", &n);
+    printf("----\n");
+    printf("Series = ");
+    for ( int i = 1 ; i <= n ; i++ )
+    {
+        ans += series;
+        if ( i <= n-1 ) {
+            printf("%d + ", series);
+        }
+        else {
+            printf("%d", series);
+        }
+        series = series * 10 + 9;
+    }
+    printf("\nSum = %d", ans);
+}
