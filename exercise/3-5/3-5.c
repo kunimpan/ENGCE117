c162f2ae36ad232982c57bb32f9d84dd8378efb3
pun <koonimpan555@gmail.com>
2022-11-26 13:41:16 +0700

    add:comment

--- exercise/3-5/3-5.c
+++ exercise/3-5/3-5.c
@@ -7,26 +7,27 @@ int main()
     printf("Input number of element to be store in array : ");
     scanf("%d", &n);
     printf("----\n");
-    for ( int i = 0 ; i < n; i++ )
-    {
+    for ( int i = 0 ; i < n; i++ ) {
         printf("Element[%d] : ", i+1);
         scanf("%d", &arr[i]);
-    }
+    } // ลูปกรอกตัวเลข
 
-    for ( int i = 0 ; i < n ; i++ ){
+    for ( int i = 0 ; i < n ; i++ ) {
 
         count = 1;
 
-        for( j = i + 1 ; j <= n-1 ; j++ ) {
+        for ( j = i + 1 ; j <= n-1 ; j++ ) {
 
-            if(arr[i] == arr[j] && arr[i] != 0){
+            if ( arr[i] == arr[j] && arr[i] != 0) {
                 count++;
                 arr[j] = 0;
-            }
-        }
+            } // เช็คตัวเลขที่ซ้ำใน array
 
-        if(arr[i] != 0){
+        } // ลูปวนตัวเลขใน array
+
+        if ( arr[i] != 0 ) {
             printf("%d -> %d\n",arr[i], count);
-        }
-    }
+        } // เช็คแสดงตัวเลข
+
+    } // ลูปใหญ่วนตัวเลขใน array
 }
